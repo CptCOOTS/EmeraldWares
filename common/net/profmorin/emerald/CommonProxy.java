c0998ca7da9deabe8806d572dfbd4bdc0ccfa0e4
ProfMorin <bmorin@vassar.edu>
2017-05-21 17:52:56 -0400

    Create Mod and Proxies

---
+++ common/net/profmorin/emerald/CommonProxy.java
@@ -0,0 +1,20 @@
+package net.profmorin.emerald;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class CommonProxy {
+
+	public void PreInit(FMLPreInitializationEvent event){
+		
+	}
+	
+	public void Init(FMLInitializationEvent event){
+		
+	}
+	
+	public void PostInit(FMLPostInitializationEvent event){
+		
+	}
+}
---
+++ common/net/profmorin/emerald/EmeraldWares.java
@@ -0,0 +1,44 @@
+package net.profmorin.emerald;
+
+import java.util.Random;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = EmeraldWares.MOD_ID, name = EmeraldWares.MOD_NAME, version = EmeraldWares.VERSION)
+public class EmeraldWares {
+	
+	//constants
+	public static final String MOD_ID = "emerald_wares";
+	public static final String MOD_NAME = "Emerald Wares";
+	public static final String VERSION = "@VERSION@";
+	public static final String RESOURCE_PREFIX = MOD_ID +":";
+	
+	//variables
+	public static Random random = new Random();
+	
+	@Instance(MOD_ID) public static EmeraldWares instance;
+	
+	@SidedProxy(clientSide = "net.profmorin.emerald.ClientProxy", serverSide = "net.profmorin.emerald.CommonProxy")
+	public static CommonProxy proxy;
+	
+	@EventHandler public void PreInit(FMLPreInitializationEvent event) {
+		
+		proxy.PreInit(event);
+	}
+	
+	@EventHandler public void Init(FMLInitializationEvent event) {
+		
+		proxy.Init(event);
+	}
+	
+	@EventHandler public void PostInit(FMLPostInitializationEvent event){
+		
+		proxy.PostInit(event);
+	}
+}
